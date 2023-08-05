75187ae91778c7097bb3e56772d437642b26b26e
Rodrigo Duran <rads2995@gmail.com>
2023-08-05 13:05:26 -0400

    DDPG example for TF.

---
+++ Environment.py
@@ -0,0 +1,69 @@
+import gymnasium as gym
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+import matplotlib.pyplot as plt
+
+problem = "Pendulum-v1"
+env = gym.make(problem)
+
+num_states = env.observation_space.shape[0]
+print("Size of State Space ->  {}".format(num_states))
+num_actions = env.action_space.shape[0]
+print("Size of Action Space ->  {}".format(num_actions))
+
+upper_bound = env.action_space.high[0]
+lower_bound = env.action_space.low[0]
+
+print("Max Value of Action ->  {}".format(upper_bound))
+print("Min Value of Action ->  {}".format(lower_bound))
+
+# This update target parameters slowly
+# Based on rate `tau`, which is much less than one.
+@tf.function
+def update_target(target_weights, weights, tau):
+    for (a, b) in zip(target_weights, weights):
+        a.assign(b * tau + a * (1 - tau))
+
+def get_actor():
+    # Initialize weights between -3e-3 and 3-e3
+    last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
+
+    inputs = layers.Input(shape=(num_states,))
+    out = layers.Dense(256, activation="relu")(inputs)
+    out = layers.Dense(256, activation="relu")(out)
+    outputs = layers.Dense(1, activation="tanh", kernel_initializer=last_init)(out)
+
+    # Our upper bound is 2.0 for Pendulum.
+    outputs = outputs * upper_bound
+    model = tf.keras.Model(inputs, outputs)
+    return model
+
+def get_critic():
+    # State as input
+    state_input = layers.Input(shape=(num_states))
+    state_out = layers.Dense(16, activation="relu")(state_input)
+    state_out = layers.Dense(32, activation="relu")(state_out)
+
+    # Action as input
+    action_input = layers.Input(shape=(num_actions))
+    action_out = layers.Dense(32, activation="relu")(action_input)
+
+    # Both are passed through seperate layer before concatenating
+    concat = layers.Concatenate()([state_out, action_out])
+
+    out = layers.Dense(256, activation="relu")(concat)
+    out = layers.Dense(256, activation="relu")(out)
+    outputs = layers.Dense(1)(out)
+
+    # Outputs single value for give state-action
+    model = tf.keras.Model([state_input, action_input], outputs)
+
+    return model
+
+def policy(state, noise_object):
+    sampled_actions = tf.squeeze(actor_model(state))
+    noise = noise_object()
+    # Adding noise to action
+    sampled_actions = sampled_actions.numpy() + noise
+
--- main.py
+++ main.py
@@ -1,33 +1,83 @@
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
+from Environment import *
+from OUActionNoise import OUActionNoise
+from Buffer import Buffer
+import numpy as np
 
-mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+# Training hyperparameters
+std_dev = 0.2
+ou_noise = OUActionNoise(mean=np.zeros(1), std_deviation=float(std_dev) * np.ones(1))
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10)
-])
+actor_model = get_actor()
+critic_model = get_critic()
 
-predictions = model(x_train[:1]).numpy()
-print(predictions)
+target_actor = get_actor()
+target_critic = get_critic()
 
-print(tf.nn.softmax(predictions).numpy())
+# Making the weights equal initially
+target_actor.set_weights(actor_model.get_weights())
+target_critic.set_weights(critic_model.get_weights())
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+# Learning rate for actor-critic models
+critic_lr = 0.002
+actor_lr = 0.001
 
-print(loss_fn(y_train[:1], predictions).numpy())
-    
-model.compile(optimizer='adam',
-    loss=loss_fn,
-    metrics=['accuracy'])
+critic_optimizer = tf.keras.optimizers.Adam(critic_lr)
+actor_optimizer = tf.keras.optimizers.Adam(actor_lr)
 
-model.fit(x_train, y_train, epochs=10)
+total_episodes = 100
+# Discount factor for future rewards
+gamma = 0.99
+# Used to update target networks
+tau = 0.005
 
-model.evaluate(x_test, y_test, verbose=2)
+buffer = Buffer(50000, 64)
 
+# To store reward history of each episode
+ep_reward_list = []
+# To store average reward history of last few episodes
+avg_reward_list = []
+
+# Takes about 4 min to train
+for ep in range(total_episodes):
+
+    prev_state = env.reset()
+    tf_prev_state = prev_state
+    episodic_reward = 0
+
+    while True:
+        # Uncomment this to see the Actor in action
+        # But not in a python notebook.
+        env.render()
+
+        action = policy(tf_prev_state, ou_noise)
+        # Recieve state and reward from environment.
+        state, reward, done, info = env.step(action)
+
+        buffer.record((prev_state, action, reward, state))
+        episodic_reward += reward
+
+        buffer.learn()
+        update_target(target_actor.variables, actor_model.variables, tau)
+        update_target(target_critic.variables, critic_model.variables, tau)
+
+        # End this episode when `done` is True
+        if done:
+            break
+
+        prev_state = state
+
+    ep_reward_list.append(episodic_reward)
+
+    # Mean of last 40 episodes
+    avg_reward = np.mean(ep_reward_list[-40:])
+    print("Episode * {} * Avg Reward is ==> {}".format(ep, avg_reward))
+    avg_reward_list.append(avg_reward)
+
+# Plotting graph
+# Episodes versus Avg. Rewards
+plt.plot(avg_reward_list)
+plt.xlabel("Episode")
+plt.ylabel("Avg. Epsiodic Reward")
+plt.show()
 
